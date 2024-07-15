34238829f99ba3b7f31bb2cb18dce54044b996ab
JazzyJak118 <kosmaja1@my.palmbeachstate.edu>
2024-07-14 23:34:25 -0400

    Update Discussion.cs

--- Module 3/Discussion.cs
+++ Module 3/Discussion.cs
@@ -1,6 +1,25 @@
 /* Working with class examples and local methods
-"Wine" and "cubes" and "cars"
+octopus, person,
 */
+
+// octopus
 using System;
 
+public class Octopus
+{  
+  {
+  public readonly string Name;
+  public int Age = 10;
+  static readonly int legs = 8, eyes = 1;
+  }
 
+ public Octopus(string name)
+    {
+        Name = name;
+    }
+  void DisplayInfo()
+ {
+   Console.WriteLine($"Name: {Name}, Age: {Age}, Legs: {Legs}");
+  }
+  
+}
